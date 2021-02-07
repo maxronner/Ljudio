1f6c247b534448660de415249521ef8d13c88af8
André <Andre.roygaard@gmail.com>
2021-02-07 22:51:28 +0100

    lade till alla bibliotek och testade dom

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router'
+import VueTour from 'vue-tour'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+import Vuelidate from 'vuelidate'
+import Vuex from 'vuex'
+require('vue-tour/dist/vue-tour.css')
+
+Vue.use(VueTour)
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(Vuelidate)
+Vue.use(Vuex)
+Vue.use(Vuesax, {
+  // options here
+})
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
