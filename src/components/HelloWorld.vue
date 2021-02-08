f5a611a77245c6a40e5aa71d49de16069b85d9e4
Saimir avdija <katundari_@hotmail.com>
2021-02-08 22:18:24 +0100

    skapade login popuppen

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,15 +27,20 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <Login></Login>
     <v-tour name="myTour" :steps="steps"></v-tour>
   </div>
 </template>
 
 <script>
+import Login from '../components/Login'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    Login
   },
       data () {
       return {
---
+++ src/components/Login.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+      <vs-button @click="active=!active">
+        Open Dialog
+      </vs-button>
+      <vs-dialog v-model="active">
+        <template #header>           
+          <h4>
+              Logga in
+          </h4>
+        </template>
+
+
+        <div>
+          <vs-input class="email-input" block v-model="email" placeholder="Email">
+          </vs-input>
+          <vs-input class="password-input" block type="password" v-model="password" placeholder="Password">
+          </vs-input>
+          <div class="flex">
+            <vs-checkbox v-model="remember">Remember me</vs-checkbox>
+            <a class="forgotpassword" href="#">Forgot Password?</a>
+          </div>
+        </div>
+
+        <template #footer>
+          <div class="footer-dialog">
+            <vs-button block>
+              Sign In
+            </vs-button>
+
+            <div class="createaccount">
+              New Here? <a href="#">Create New Account</a>
+            </div>
+          </div>
+        </template>
+      </vs-dialog>
+    </div>
+  </template>
+
+<script>
+ export default {
+    name: 'Login',
+data:() => ({
+        active: false,
+        email: '',
+        password: '',
+        remember: false
+      })
+}
+</script>
+
+<style scoped>
+    .flex{
+        justify-content: space-between;
+        display: flex;
+        align-items: center;
+    }
+    .forgotpassword{
+        text-decoration: none;
+        font-size: .9rem;
+    }
+    .createaccount{
+        font-size: .9rem;
+    }
+    .email-input{
+        margin-bottom: 5%;
+    }
+    .password-input{
+        margin-bottom: 5%;
+    }
+    #popup{
+        background-color:#262729;
+    }
+</style>
