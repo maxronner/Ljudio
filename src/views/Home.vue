8459941f10bcf200e6a83c01e9db01e9ff6e45bf
Max Ronner <contact@ronner.dev>
2021-02-08 11:57:32 +0100

    FIX: missing file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sidebar/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Sidebar from '../components/partials/sidebar'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Sidebar
   }
 }
 </script>
