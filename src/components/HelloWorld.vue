488ae2db64a26529bed133df9a1006feffcebd56
André <Andre.roygaard@gmail.com>
2021-02-08 21:59:10 +0100

    test av iframe och gjorde klart första sidan för komponenterna.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,32 @@
 <template>
-  <div class="hello">
-    <h1 id="v-step-0" >{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul id="v-step-1">
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3 id="v-step-2">Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-    <v-tour name="myTour" :steps="steps"></v-tour>
+  <div >
+  <vs-button @click="active=!active">
+        Open Dialog
+      </vs-button>
+  <vs-dialog v-model="active">
+  <div>
+   <iframe id="ytplayer" type="text/html" width="720" height="405"
+src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&disablekb=1&enablejsapi=1&loop=1&iv_load_policy=3"
+frameborder="0" allowfullscreen></iframe>
+  </div>
+</vs-dialog>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  components:{
+
+  },
   props: {
     msg: String
   },
       data () {
       return {
+        active: false,
+        num: 2,
+          num2: 10,
         steps: [
           {
             target: '#v-step-0',  // We're using document.querySelector() under the hood
